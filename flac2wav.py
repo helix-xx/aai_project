f7c8e6f9ca1027fbef6836c9f8a5744fbd860302
helix_xx <630824252@qq.com>
2022-12-25 20:28:43 +0800

    flac to wav

---
+++ flac2wav.py
@@ -0,0 +1,37 @@
+import os
+
+# flac to wav all
+# need_convert=["noise","test","test_noise","train"]
+need_convert=["train"]
+root_path="/mnt/f/workspace/AAI_project/LibriSpeech-SI/"
+# for _ in need_convert:
+#     print(root_path+_+'/')
+def listdir(path, list_name,prefix_len):
+    for file in os.listdir(path):
+        file_path = os.path.join(path,file)
+        if os.path.isdir(file_path):
+            listdir(file_path, list_name,prefix_len)
+        else:
+            list_name.append(file_path[prefix_len:])
+
+# listdir(root_path+need_convert[3]+'/',all_path)
+for convert in need_convert:
+    all_path=[]
+    listdir(root_path+convert+'/',all_path,len(root_path+convert+'/'))
+    sizes=len(all_path)
+    item_name=convert
+    y_cnt=0
+    n_cnt=0
+    for originpath in all_path:
+        convertpath = root_path+convert+"_wav/"+originpath.split('.')[0]+".wav"
+        originpath = root_path+convert +"/" + originpath
+        if dir != "/".join(convertpath.split('/')[0:-1]):
+            dir = "/".join(convertpath.split('/')[0:-1])
+            if os.path.exists(dir) is False:
+                os.makedirs(dir)
+        if os.path.exists(convertpath) is False:
+            os.system('ffmpeg -i %s %s > /dev/null' % (originpath,convertpath))
+            y_cnt=y_cnt+1
+        else:
+            n_cnt=n_cnt+1
+    print("正在转换%s,已完成%s,共计%s" % (item_name,y_cnt,sizes))
